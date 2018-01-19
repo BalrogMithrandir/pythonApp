e7dca744dd5618aea44c748af9f05d5c21d620b7
zhushuanghe <zhushuanghe@ablecloud.cn>
2018-01-19 19:21:47 +0800

    uopdate

--- paraTheTimeInfoOfWiFiMsgStream.py
+++ paraTheTimeInfoOfWiFiMsgStream.py
@@ -1,6 +1,4 @@
 # -*- coding: UTF-8 -*-
-from pandas import Series, DataFrame
-import pandas as pd
 import time
 import binascii
 
@@ -25,159 +23,144 @@ WiFiOfflineReason = ["Poweroff", "Reboot", "RecvErr", "WiFiDisconnect", "HeartBe
 
 def ParaMsgStreamTimeInfo(TimeStampInfo):
 	
-	print "----****\tmessage stream information of WiFi\t****----\n"
+	print("----****\tmessage stream information of WiFi\t****----\n")
 	
 	index = 0
-	for i in range(0, len(TimeStampInfo)):
-		if i%2 != 0:
-			continue
-		if int(TimeStampInfo[i:i+2],16) == WiFiRmtDbgType.eZCWRDT_GetMsgStreamInfo:
-			i+=2
-			break;
-	index = i
-		
-	print len(TimeStampInfo)
-	MsgNum = int(TimeStampInfo[index:index+2], 16)
-	print "Msg number: %d\n\n" %(MsgNum)
+	
+	MsgNum = TimeStampInfo[index]
+	print("Msg number: %d\n\n" %(MsgNum))
 	index+=2
 	
 	for msgIndex in range(0, MsgNum):
-		print "\nMsg %d Info" %(msgIndex)
+		print("\nMsg %d Info" %(msgIndex))
 		
-		MsgSn = int(TimeStampInfo[index:index+2], 16)
-		print "\tMsgSn is ", MsgSn
-		index+=2
+		MsgSn = TimeStampInfo[index]
+		print("\tMsgSn is ", MsgSn)
+		index+=1
 		
-		MsgDir = int(TimeStampInfo[index:index+2], 16)
-		print "\tMsgDir is ", MsgDirection[MsgDir]
-		index+=2
+		MsgDir = TimeStampInfo[index]
+		print("\tMsgDir is ", MsgDirection[MsgDir])
+		index+=1
 		
-		timeStamp = int(TimeStampInfo[index:index+8], 16)
-		index+=8
+		TimeStamp = TimeStampInfo[index]*pow(2,8*3) + TimeStampInfo[index+1]*pow(2,8*2) + TimeStampInfo[index+2]*pow(2,8) + TimeStampInfo[index+3]
+		timeArray = time.localtime(TimeStamp)		
+		index+=4
 
 		timeArray = time.localtime(timeStamp)
 		otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
-		print "\ttimeStamp is ", otherStyleTime
+		print("\ttimeStamp is ", otherStyleTime)
 
 def ParaRedirectorInfo(RedirectorInfo):
-	print "----**** redirector information ****----\n"
-	print "total len is %d\n" %(len(RedirectorInfo))
+	print("----**** redirector information ****----\n")
 	
 	index = 0
-	for i in range(0, len(RedirectorInfo)):
-		if i%2 != 0:
-			continue
-		if int(RedirectorInfo[i:i+2],16) == WiFiRmtDbgType.eZCWRDT_GetRedirectorInfo:
-			i+=2
-			break;
-	index = i
 	
-	DomainNameLen = int(RedirectorInfo[index:index+2], 16)
-	print "domain name len is %d\n" %(DomainNameLen)
+	DomainNameLen = RedirectorInfo[index]
+	print("domain name len is %d\n" %(DomainNameLen))
+	index+=1
+	
+	PublicKeyLen = RedirectorInfo[index]
+	print("cloud public key len is %d\n" %(PublicKeyLen))
+	index+=1	
+
+	Port_int = RedirectorInfo[index]*256 + RedirectorInfo[index+1]
+	print("redirector port is %d\n" %(Port_int))
 	index+=2
 	
-	PublicKeyLen = int(RedirectorInfo[index:index+2], 16)
-	print "cloud public key len is %d\n" %(PublicKeyLen)
-	index+=2	
-
-	Port_int = int(RedirectorInfo[index:index+4], 16)
-	print "redirector port is %d\n" %(Port_int)
-	index+=4
+	DomainName = "".join(chr(ascInt) for ascInt in  RedirectorInfo[index:index+DomainNameLen])
+	print("redirector's domain name is %s \n\t"  %(DomainName))
+	index+=DomainNameLen
 	
-	DomainName = binascii.a2b_hex(RedirectorInfo[index:index+DomainNameLen*2])
-	print "redirector's domain name is %s \n\t"  %(DomainName)
-	index+=DomainNameLen*2
-	
-	print "redirector's public key is %s \n\t"  %(RedirectorInfo[index:index+PublicKeyLen*2])
+	Publickey = []
+	for i in range(index, index+PublicKeyLen):
+		Publickey.append(hex(RedirectorInfo[i]))
+	print("redirector's public key is %s \n\t"  %(Publickey))
 	
 	
-		
 def ParaRebootAndOfflineInfo(RebootAndOfflineInfo):
-	print "----****\treboot and offline information of WiFi\t****----\n"
-	print len(RebootAndOfflineInfo)
+	print("----****\treboot and offline information of WiFi\t****----\n")
 	
 	index = 0
-	for i in range(0, len(RebootAndOfflineInfo)):
-		if i%2 != 0:
-			continue
-		if int(RebootAndOfflineInfo[i:i+2],16) == WiFiRmtDbgType.eZCWRDT_GetRbtAndOfflineInfo:
-			i+=2
-			break;
-	index = i
 	
-	RebootInfoNum = int(RebootAndOfflineInfo[index:index+2], 16)
-	print "reboot info number: %d\n\n" %(RebootInfoNum)
-	index+=2
+	RebootInfoNum = RebootAndOfflineInfo[index]
+	print("reboot info number: %d\n\n" %(RebootInfoNum))
+	index += 1
 
-	OfflineInfoNum = int(RebootAndOfflineInfo[index:index+2], 16)
-	print "offline info number: %d\n\n" %(OfflineInfoNum)
-	index+=2
+	OfflineInfoNum = RebootAndOfflineInfo[index]
+	print("offline info number: %d\n\n" %(OfflineInfoNum))
+	index += 1
 	
-	RebootInfoIndex = int(RebootAndOfflineInfo[index:index+2], 16)
-	print "reboot info index: %d\n\n" %(RebootInfoIndex)
-	index+=2
+	RebootInfoIndex = RebootAndOfflineInfo[index]
+	print("reboot info index: %d\n\n" %(RebootInfoIndex))
+	index += 1
 	
-	OfflineInfoIndex = int(RebootAndOfflineInfo[index:index+2], 16)
-	print "offline info index: %d\n\n" %(OfflineInfoIndex)
-	index+=2
+	OfflineInfoIndex = RebootAndOfflineInfo[index]
+	print("offline info index: %d\n\n" %(OfflineInfoIndex))
+	index += 1
 	
 	for RebootIndex in range(0, RebootInfoNum):
-		print "Reboot info %d Info \n" %(RebootIndex)
+		print("Reboot info %d Info \n" %(RebootIndex))
 		
-		TimeStamp = int(RebootAndOfflineInfo[index:index+8], 16)
+		TimeStamp = RebootAndOfflineInfo[index]*pow(2,8*3) + RebootAndOfflineInfo[index+1]*pow(2,8*2) + RebootAndOfflineInfo[index+2]*pow(2,8) + RebootAndOfflineInfo[index+3]
 		timeArray = time.localtime(TimeStamp)
 		otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
-		print "\ttimeStamp is ", otherStyleTime		
-		index+=8
+		print("\ttimeStamp is ", otherStyleTime)
+		index+=4
 		
 		#ReabootReason info: 1Byte
-		RbtReason = int(RebootAndOfflineInfo[index:index+2], 16)
-		print "\tRbtReason is ", WiFiRebootReason[RbtReason] 
-		index+=2
+		RbtReason = RebootAndOfflineInfo[index]
+		print("\tRbtReason is ", WiFiRebootReason[RbtReason])
+		index+=1
 		
 		#extra info: 1Byte
-		ExtraInfo = int(RebootAndOfflineInfo[index:index+2], 16)
-		print "\tExtra is ", ExtraInfo
-		index+=2
+		ExtraInfo = RebootAndOfflineInfo[index]
+		print("\tExtra is ", ExtraInfo)
+		index+=1
 		
 	for OfflineIndex in range(0, OfflineInfoNum):
-		print "offline info %d Info \n" %(OfflineIndex)
+		print("offline info %d Info \n" %(OfflineIndex))
 		
 		#timestamp: 4Byte
 		
-		TimeStamp = int(RebootAndOfflineInfo[index:index+8], 16)
+		TimeStamp = RebootAndOfflineInfo[index]*pow(2,8*3) + RebootAndOfflineInfo[index+1]*pow(2,8*2) + RebootAndOfflineInfo[index+2]*pow(2,8) + RebootAndOfflineInfo[index+3]
 		timeArray = time.localtime(TimeStamp)
 		otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", timeArray)
-		print "\ttimeStamp is ", otherStyleTime	
+		print("\ttimeStamp is ", otherStyleTime)
 		
-		index+=8		
+		index+=4
 		
 		#offline Reason: 1Byte
-		OfflineReason = int(RebootAndOfflineInfo[index:index+2], 16)
-		print "\tRbtReason is ", WiFiOfflineReason[OfflineReason] 
-		index+=2
+		OfflineReason = RebootAndOfflineInfo[index]
+		print("\tRbtReason is ", WiFiOfflineReason[OfflineReason])
+		index+=1
 		
 		#extra info: 1Byte
-		ExtraInfo = int(RebootAndOfflineInfo[index:index+2], 16)
-		print "\tExtra is ", ExtraInfo
-		index+=2
+		ExtraInfo = RebootAndOfflineInfo[index]
+		print("\tExtra is ", ExtraInfo)
+		index+=1
 		
 		
 if __name__ == '__main__':
 	
-	DbgInfo = "01061124238c746573742e61626c65636c6f75642e636eb8d935e4d6d8f2d6c8282f9fd96248c796a9ed5b5a1a9559d23cbb5f1b03073f01000100"
+	DbgInfoStr = "01061124238c746573742e61626c65636c6f75642e636eb8d935e4d6d8f2d6c8282f9fd96248c796a9ed5b5a1a9559d23cbb5f1b03073f01000100"
+	n = 0
+	DbgInfoInt = []
+	while n < len(DbgInfoStr):
+		DbgInfoInt.append(int(DbgInfoStr[n:n+2], 16))
+		n += 2
+	print(DbgInfoInt)
 	index = 0
-	for i in range(0, len(DbgInfo)):
-		if DbgInfo[i] == '1':
+	for i in range(0, len(DbgInfoInt)):
+		if DbgInfoInt[i] == 1:
 			break;
 	index = i+1
-	DbgType = int(DbgInfo[index+1], 16)
+	DbgType = DbgInfoInt[index]
 	
 	if DbgType == WiFiRmtDbgType.eZCWRDT_GetMsgStreamInfo:
-		ParaMsgStreamTimeInfo(DbgInfo)
+		ParaMsgStreamTimeInfo(DbgInfoInt[index+1:])
 	elif DbgType == WiFiRmtDbgType.eZCWRDT_GetRedirectorInfo:
-		ParaRedirectorInfo(DbgInfo)		
+		ParaRedirectorInfo(DbgInfoInt[index+1:])
 	elif DbgType == WiFiRmtDbgType.eZCWRDT_GetRbtAndOfflineInfo:
-		ParaRebootAndOfflineInfo(DbgInfo)
+		ParaRebootAndOfflineInfo(DbgInfoInt)
 	else:
-		print "invalid debug type %d" %(DbgType)
+		print("invalid debug type %d" %(DbgType))
